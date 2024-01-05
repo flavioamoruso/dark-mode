d304fb68480f771dbe532984810d7201f91b847f
FLAVIO AMORUSO <flavioamoruso6.com>
2024-01-05 17:48:57 +0100

    completato

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import data from "./data";
+import Articolo from "./Articolo";
+
+//Funzione che se presente 'Theme' nel localStorage
+
+const getFromLocalStorage=()=>{
+
+  if(localStorage.getItem('theme')){
+    return localStorage.getItem('theme');
+  }else{
+    return 'light-mode'
+  }
+
+}
+
+// returna il suo valore o di default return 'light-mode'
 
 function App() {
+
+  const[theme,setTheme]=useState(getFromLocalStorage());
+
+  // funzione che cambia il tema
+
+  const cambiaTema=()=>{
+    if(theme==='light-mode'){
+    setTheme('dark-mode')
+  }else{
+    setTheme('light-mode')
+  }
+}
+
+// Al mutare del theme state,attacca una classe al nostro HTML tag
+useEffect(()=>{
+  document.documentElement.className=theme;
+  localStorage.setItem("theme", theme);
+},[theme]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <div className="container">
+        <button onClick={cambiaTema} className="btn">
+          Cambia
+        </button>
+        <section className="article-section">
+          {
+            data.map(el=> <Articolo key={el.id} {...el}/>) 
+          }
+        </section>
+      </div>
+    </section>
   );
 }
 
